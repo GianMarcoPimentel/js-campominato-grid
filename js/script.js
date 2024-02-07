f30d25602a3daec977aa4a441d3bcf4b66101ae1
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-07 17:25:17 +0100

    Creazione della griglia con numeri causali

--- js/script.js
+++ js/script.js
@@ -21,7 +21,8 @@ buttonElement.addEventListener("click", function(){
 
     const gridElement = document.querySelector("#grid");
     /* console.log(gridElement); */
-    
+    const RandomNumbersArray = getRandomNumbersArray();
+    console.log(RandomNumbersArray);
     //griglia 10x10
     for (let i = 0; i < 100 ; i++){
         // per ognunodi questi creare un elemento 
@@ -29,15 +30,50 @@ buttonElement.addEventListener("click", function(){
         /* console.log(newElement); */
         // ci aggiungo la classe
         newElement.classList.add("square");
-        //all'interno di se stesso ci aggiungo il nostro newElement
-        gridElement.append(newElement);
+        newElement.innerText = RandomNumbersArray[i]; // per scrivere all'interno degli square i numeri da 1 a 100 ORDINATI
+        
         newElement.addEventListener("click", function(){
             /* console.log("click"); */
             console.log(this);
             //per mezzo di 'this' aggiungo una classe
             this.classList.toggle("active");
-        
+
+            newElement.classList.add("even");
+            newElement.classList.add("odd");
     })
+
+        //all'interno di se stesso ci aggiungo il nostro newElement
+        gridElement.append(newElement);
+
     }
 
 })
+
+//faccio la funzione per creare i numeri casuali all'interno degli square
+
+function getRandomNumber(maxNumber){
+
+    const randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+    return randomNumber;
+
+}
+getRandomNumbersArray();
+function getRandomNumbersArray(){
+
+    const numbersArray = [];
+
+    while(numbersArray.length < 100){
+    /* numbersArray.push(getRandomNumber(100)); */        
+        /* console.log(numbersArray); */
+        // insrisci il numero solo se non è già presente
+        const newNumber = getRandomNumber(100) ;
+        //controllo se il numero è già presente ne nostro array
+        if( ! numbersArray.includes(newNumber)){
+            numbersArray.push(newNumber);
+        }
+    
+    }
+     // per farlo apparire in pagina devo fare il return del mio array
+     return numbersArray;
+}
